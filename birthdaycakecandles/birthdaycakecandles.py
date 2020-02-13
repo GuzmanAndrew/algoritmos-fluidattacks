f92e17b1e136a6c6f31a04d428b8ca0c6ab65dbc
guzmanandrew <ramirezandrew172@gmail.com>
2020-02-13 15:48:38 -0500

    sol(code): #0 hackerrank, birthdaycakecand (easy)
    - others: 0 in, 5 out, 5 total.
    - score: 01.00 initial, 01.65 final, 0.65 progress.
    - global-rank: 2961 initial, 2261 final, 700 progress.
    - national-rank: 2340 initial, 2262 final, 78 progress.
    - effort: 20 hours during challenges, productivity 0.0325 points/hour.

---
+++ birthdaycakecandles/birthdaycakecandles.py
@@ -0,0 +1,15 @@
+"""
+$ pylint birthday_cake_candles.py
+Your code has been rated at 10.00/10 (previous run: 5.00/10, +5.00)
+
+"""
+
+def birthday_cake_candles(ar_one):
+    """Function for validation of candles."""
+    print ar_one.count(82)
+
+
+birthday_cake_candles([82, 49, 82, 82, 41, 82, 15, 63, 38, 25])
+
+# $ python birthday_cake_candles.py
+# 4

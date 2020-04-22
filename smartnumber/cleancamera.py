f3371625bf9df4fd01798c5b47862ae7cddfc4a6
Andrew Guzman <ramirezandrew172@gmail.com>
2020-04-22 00:19:27 -0500

    Reto code

---
+++ smartnumber/cleancamera.py
@@ -0,0 +1,34 @@
+"""
+$ pylint cleancamera.py.py
+Your code has been rated at 10.00/10 (previous run: 5.00/10, +5.00)
+"""
+
+import math
+
+FILE_OPEN = open('DATA.lst', 'r')
+MESSAGE = FILE_OPEN.read()
+
+DATA = [MESSAGE] 
+
+def is_smart_number(num):
+    val = int(math.sqrt(num))
+    if num / val == val:
+        return True
+    return False
+
+for line in DATA:
+    TYPE_DATA = line.split()
+    DATA_INT = list(map(int, TYPE_DATA))
+
+for line in DATA_INT:
+    num = line
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")
+
+FILE_OPEN.close()
+
+# $ python cleancamera.py
+# 7

3504f60cb69c8b92fbfc5ca65be1952bcaba899a
Andrew Ramirez <ramirezandrew172@gmail.com>
2020-06-13 20:51:14 -0500

    Modificacion python

--- Code/HackerRank/xorstrings/cleancamera.py
+++ Code/HackerRank/xorstrings/cleancamera.py
@@ -1,21 +1,26 @@
+#!/usr/bin/env python3
+
+# El comando de arriba sirve para soluciona los errores 1 y 2 de abajo
+#1 = ./cleancamera.py: line 8: syntax error near unexpected token `('
+#2 = ./cleancamera.py: line 8: `FILE_OPEN = open("DATA.lst", "r")'
+
 """
 $ pylint cleancamera.py.py
 Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00)
 """
 
-FILE_OPEN = open('DATA.lst', 'r')
-MESSAGE = FILE_OPEN.read()
-ARRAY_ONE = MESSAGE[5:10]
-ARRAY_SECOND = MESSAGE[0:5]
-
-
-def strings(one_param: str, second_param: str) -> str:
+def strings():
     """
     comparison of arrays
     """
+    file_open = open("DATA.lst", "r")
+    data_array = file_open.read()
+    array_one = data_array[5:10]
+    array_second = data_array[0:5]
+
     array_final = ""
     for i in range(0, 5):
-        if one_param[i] == second_param[i]:
+        if array_one[i] == array_second[i]:
             array_final += '0'
         else:
             array_final += '1'
@@ -23,11 +28,9 @@ def strings(one_param: str, second_param: str) -> str:
     return array_final
 
 
-RESULT_FUNCTION = strings(ARRAY_ONE, ARRAY_SECOND)
+RESULT_FUNCTION = strings()
 
 print(RESULT_FUNCTION)
 
-
 # $ python cleancamera.py
 # 10000
-
